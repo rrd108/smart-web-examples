04588558bf4fadb8abf752f40bd9c615d4b8ffc9
rrd <rrd@1108.cc>
2022-01-30 13:56:51 +0100

    dark mode finalized

--- 05-dark-mode/theme-switcher.js
+++ 05-dark-mode/theme-switcher.js
@@ -1,17 +1,31 @@
 const doc = document.getElementsByTagName('html')[0]
-const light = document.querySelector('#light')
-const dark = document.querySelector('#dark')
+const light = document.getElementById('light')
+const dark = document.getElementById('dark')
 
 const themeSwitcher = theme => {
   if (theme == 'light') {
     light.style.display = 'none'
     dark.style.display = 'block'
-    return doc.classList.add('dark-theme')
+    doc.classList.add('dark-mode')
+    localStorage.setItem('theme', 'dark')
+    return
   }
-  doc.classList.remove('dark-theme')
+
   light.style.display = 'block'
   dark.style.display = 'none'
+  doc.classList.remove('dark-mode')
+  localStorage.setItem('theme', 'light')
 }
 
 light.addEventListener('click', () => themeSwitcher('light'))
 dark.addEventListener('click', () => themeSwitcher('dark'))
+
+if (localStorage.getItem('theme')) {
+  themeSwitcher(localStorage.getItem('theme') == 'dark' ? 'light' : 'dark')
+}
+
+if (!localStorage.getItem('theme')) {
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? themeSwitcher('light')
+    : themeSwitcher('dark')
+}
